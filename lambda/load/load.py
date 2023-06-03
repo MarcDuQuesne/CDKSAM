86dfb67b98ca5c0b63ea60a89a68ffad07145b54
Matteo Giani <matteo.giani.87@gmail.com>
2023-06-03 09:28:56 +0000

    Modified the load function.

--- lambda/load/load.py
+++ lambda/load/load.py
@@ -5,6 +5,8 @@ This lambda downloads the transformed data package, validates it and then loads 
 import os
 from typing import Dict
 
+import boto3
+
 
 def handler(event: Dict, context: Dict) -> Dict:
     """
@@ -22,4 +24,11 @@ def handler(event: Dict, context: Dict) -> Dict:
     except KeyError as exc:
         raise ValueError("env not correctly set up.") from exc
 
-    return {"success": "True"}
+    # Create an S3 client
+    s3 = boto3.client("s3")
+    # List the contents of the bucket
+    response = s3.list_objects_v2(Bucket=bucket_name)
+
+    print({"bucket": bucket_name, "content": response["Contents"]})
+
+    return {"result": "success"}
