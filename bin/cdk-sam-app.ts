2234c1c1b622b66fd55529c15c762b7058fbd350
Matteo Giani <matteo.giani.87@gmail.com>
2023-06-11 08:55:49 +0000

    Fixing the localstack test.

--- bin/cdk-sam-app.ts
+++ bin/cdk-sam-app.ts
@@ -5,11 +5,9 @@ import * as cdk from "aws-cdk-lib";
 import { CDK2SAMStack } from "../lib/cdk-sam";
 
 const app = new cdk.App();
-new CDK2SAMStack(app, "CDK2SAMStack",
-    {
-        env: {
-            account: process.env.CDK_DEFAULT_ACCOUNT,
-            region: process.env.CDK_DEFAULT_REGION,
-        },
-    }
-);
+new CDK2SAMStack(app, "CDK2SAMStack", {
+  env: {
+    account: "111111",
+    region: "eu-west-1",
+  },
+});
