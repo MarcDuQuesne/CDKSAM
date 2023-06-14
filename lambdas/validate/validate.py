021b8f1678094ab9320914337837558a270f4ebf
Matteo Giani <matteo.giani.87@gmail.com>
2023-06-14 08:41:32 +0000

    A more significant validate function

--- lambdas/validate/validate.py
+++ lambdas/validate/validate.py
@@ -1,33 +1,47 @@
 """
-This lambda downloads the transformed data package, validates it and then loads it into the data warehouse.
+Lambda function to validate files uploaded to S3
 """
 
 import json
-import os
+import logging
 from typing import Dict
 
 import boto3
+import pandas as pd
 import pandera as pa
 
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
-def handler(event: Dict, context: Dict) -> Dict:
+s3_client = boto3.client('s3')
+
+# create a DataFrameSchema object that can be used to validate a DataFrame
+schema =  pa.DataFrameSchema({
+            "temperature": pa.Column(pa.Float),
+            "humidity": pa.Column(pa.Float, checks=[
+                pa.Check.greater_than_or_equal_to(0),
+                pa.Check.less_than_or_equal_to(100)
+                ]),
+            }, index=pa.Index(pa.DateTime))
+
+def validate_csv_file(event: Dict, context: Dict) -> Dict[str, str]:
     """
-    This lambda downloads the transformed data package, validates it and then loads it into the data warehouse.
+    Validate the CSV file uploaded to S3
     """
 
-    print(f"event: {event}")
-    print(f"context: {context}")
+    print(event, context)
 
-    try:
-        bucket_name, sqlalchemy_url = (
-            os.environ.get("BUCKET_NAME"),
-            os.environ.get("SQLALCHEMY_URL"),
-        )
-    except KeyError as exc:
-        raise ValueError("env not correctly set up.") from exc
+    # Get the object from the event and show its content type
+    bucket_name = event['Records'][0]['s3']['bucket']['name']
+    file_info = event['Records'][0]['s3']
+    file_key = file_info['object']['key']
 
-    # Create an S3 client
-    s3 = boto3.client("s3")
-    ## TODO Validation logic
+    # read file from S3 into DataFrame
+    csv_obj = s3_client.get_object(Bucket=bucket_name, Key=file_key)
+    body = csv_obj["Body"]
+    df = pd.read_csv(body, index_col='datetime', parse_dates=True)
 
+    # if validation does not pass,
+    # a SchemaErrors object is raised with a detailed explanation of the error
+    schema.validate(df)
     return {"statusCode": 200, "body": json.dumps("Success!!")}
---
+++ tests/test_validate.py
@@ -0,0 +1,82 @@
+""" Tests for the validatation lambda """
+import os
+import sys
+
+import boto3
+import moto
+import pytest
+
+# Get the current directory
+current_dir = os.path.abspath(os.path.dirname(__file__))
+
+# Add the lambdas directory to the path
+sys.path.append(os.path.join(current_dir, ".."))
+
+from lambdas.validate.validate import validate_csv_file
+
+
+@pytest.fixture()
+def s3_event():
+    """Generates S3 Event"""
+
+    return {
+        "Records": [
+            {
+                "eventVersion": "2.0",
+                "eventSource": "aws:s3",
+                "awsRegion": "us-east-1",
+                "eventTime": "1970-01-01T00:00:00.000Z",
+                "eventName": "ObjectCreated:Put",
+                "userIdentity": {"principalId": "EXAMPLE"},
+                "requestParameters": {"sourceIPAddress": "0.0.0.0"},
+                "responseElements": {
+                    "x-amz-request-id": "EXAMPLE123456789",
+                    "x-amz-id-2": "EXAMPLE123/5678abcdefghijklambdaisawesome/mnopqrstuvwxyzABCDEFGH",
+                },
+                "s3": {
+                    "s3SchemaVersion": "1.0",
+                    "configurationId": "testConfigRule",
+                    "bucket": {
+                        "name": "validatetestbucket",
+                        "ownerIdentity": {"principalId": "EXAMPLE"},
+                        "arn": "arn:aws:s3:::validatetestbucket",
+                    },
+                    "object": {
+                        "key": "test.csv",
+                        "size": 1024,
+                        "eTag": "d41d8cd98f00b204e9800998ecf8427e",
+                        "versionId": "096fKKXTRTtl3on89fVO.nfljtsv6qko",
+                    },
+                },
+            }
+        ]
+    }
+
+
+@moto.mock_s3
+@pytest.fixture(autouse=True)
+def initialize_s3_bucket():
+    """Initialize S3 bucket"""
+
+    # create a fake bucket and upload a file to it
+    s3 = boto3.resource("s3")
+    s3.create_bucket(Bucket="validatetestbucket")
+    s3.Object("validatetestbucket", "test.csv").put(Body=open(os.path.join(current_dir, "data/dataset.csv"), "rb"))
+
+
+def test_validate_csv_file(s3_event):
+    """Test validate_csv_file"""
+
+    # call the lambda function handler
+    result = validate_csv_file(s3_event, "")
+
+    # check that the result is correct
+    assert result["statusCode"] == 200, "Status code is not 200"
+
+
+if __name__ == "__main__":
+    pytest.main(
+        [
+            __file__,
+        ]
+    )
